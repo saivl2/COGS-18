6e478945cf6e3a2a6740f0f954d11505b52509bd
SaiV03 <supriyavadali123@gmail.com>
2024-11-03 09:49:29 +0530

    second commit

---
+++ Assignments/A1Code/q10_code.py
@@ -0,0 +1,10 @@
+
+# YOUR CODE HERE
+if operation == 'add':
+    answer = num1 + num2
+elif operation == 'subtract':
+    answer = num1- num2
+elif operation == 'multiply':
+    answer = num1 * num2
+elif operation == 'divide':
+    answer = num1 / num2
---
+++ Assignments/A1Code/q8_code.py
@@ -0,0 +1,11 @@
+# ^ You can ignore this line - it is used to help check your code
+
+# YOUR CODE HERE
+if check_1==True and check_2 == True:
+    outcome = 'BOTH'
+elif check_1 == True and check_2 == False:
+    outcome = 'ONE'
+elif check_1 == False and check_2 == True:
+    outcome = 'TWO'
+else:
+    outcome = 'NEITHER'
